4875865bd831f71af2ebd50469b062f311fc267b
vibhav chaturvedy <vebzchaturvedy@gmail.com>
2017-06-23 01:43:33 +0530

    admin features added

--- labquiz/teacher/admin.py
+++ labquiz/teacher/admin.py
@@ -5,5 +5,14 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
+class PostModelAdmin(admin.ModelAdmin): #customizing admin
+	list_display = ["question"]
+	
+	list_filter = ["question"] #filter the fields
+	search_fields = ["question"] #search the fields
+	#list_editable = ["question"] #we can edit the fields like here title
+	class Meta:
+		model = Question #adding post model to PostAdmin
+
 admin.site.register(Exam)
-admin.site.register(Question)
+admin.site.register(Question,PostModelAdmin)
