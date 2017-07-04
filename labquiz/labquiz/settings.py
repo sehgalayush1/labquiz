6bc7d792e907403b52da476745c12d561806347b
saurabh ranjan <saurabhranjan1806@gmail.com>
2017-07-04 14:30:38 +0530

    added student

--- labquiz/labquiz/settings.py
+++ labquiz/labquiz/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'teacher',
     'login',
+    'student',
     'multiselectfield',
 
 ]
--- labquiz/labquiz/urls.py
+++ labquiz/labquiz/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('login.urls')),
     url(r'^teacher/', include('teacher.urls', namespace='teacher')),
+    # url(r'^student/', include('student.urls'))
 
 ]
