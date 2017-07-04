8b7bbb11a4ef37b317334f9ca0e0c2d0b31e3a03
saurabh ranjan <saurabhranjan1806@gmail.com>
2017-07-04 14:35:40 +0530

    teacher authentication corrected

--- labquiz/teacher/views.py
+++ labquiz/teacher/views.py
@@ -27,62 +27,78 @@ def index(request):
 
 
 def addExam(request):
-	if request.method == "POST":
-		form = AddExam(request.POST)
-		if form.is_valid():
-			form.save()
-			return HttpResponseRedirect('/teacher/')
+	if request.username.is_authenticated():
+		if request.method == "POST":
+			form = AddExam(request.POST)
+			if form.is_valid():
+				form.save()
+				return HttpResponseRedirect('/teacher/')
+		else:
+			form = AddExam()
+		return render(request, 'teacher/addExam.html', {'form':form})
 	else:
-		form = AddExam()
-	return render(request, 'teacher/addExam.html', {'form':form})
+		return HttpResponseRedirect('/')
 
 
 def addQuestion(request):
-	if request.method == "POST":
-		form = InsertQuestions(request.POST)
-		if form.is_valid():
-			f = form.save(commit= False)
-			f.exam = Exam.objects.get(pk=examId)
-			f.save()
-			messages.success(request, 'New question added!')
-			return HttpResponseRedirect(reverse_lazy('teacher:addQuestion')) #needs to be redirected to the previous page where more can be added easily.
-		else:
-			return render(request, 'error.html')
-	form = InsertQuestions()
-	return render(request, 'teacher/insertQues.html',{'form': form})
+	if request.user.is_authenticated():
+		if request.method == "POST":
+			form = InsertQuestions(request.POST)
+			if form.is_valid():
+				f = form.save(commit= False)
+				f.exam = Exam.objects.get(pk=examId)
+				f.save()
+				messages.success(request, 'New question added!')
+				return HttpResponseRedirect(reverse_lazy('teacher:addQuestion')) #needs to be redirected to the previous page where more can be added easily.
+			else:
+				return render(request, 'error.html')
+		form = InsertQuestions()
+		return render(request, 'teacher/insertQues.html',{'form': form})
+	else:
+		return HttpResponseRedirect('/')
 	
 
 
 def allQuestions(request, id):
-	exam = Exam.objects.get(pk=id)
-	global examId
-	examId = id
-	return render(request, 'teacher/displayques.html', {'exam': exam})
+	if request.user.is_authenticated():
+		exam = Exam.objects.get(pk=id)
+		global examId
+		examId = id
+		return render(request, 'teacher/displayques.html', {'exam': exam})
+	else:
+		return HttpResponseRedirect('/')
 
 
 def editQuestion(request, id):
-	n = Question.objects.get(id=id)
-	if request.method == "POST":
-		form = InsertQuestions(request.POST,instance=n)
-		if form.is_valid():
-			form.save()
-			exam = Exam.objects.get(pk=id)
-			return render(request, 'teacher/displayques.html', {'exam': exam})
+	if request.user.is_authenticated():
+		n = Question.objects.get(id=id)
+		if request.method == "POST":
+			form = InsertQuestions(request.POST,instance=n)
+			if form.is_valid():
+				form.save()
+				exam = Exam.objects.get(pk=id)
+				return render(request, 'teacher/displayques.html', {'exam': exam})
+		else:
+			form = InsertQuestions(instance=n)
+		return render(request,'teacher/editQues.html', {'form': form})
 	else:
-		form = InsertQuestions(instance=n)
-	return render(request,'teacher/editQues.html', {'form': form})
+		return HttpResponseRedirect('/')
+
 
 def addStaff(request):
-	if request.method=='POST':
-		username = request.POST['username']
-		password = request.POST['password']
-		print password
-		user = User.objects.create_user(username = username, password = password)
-		user.save()
-		return HttpResponseRedirect('/teacher/addStaff/')
+	if request.user.is_authenticated():
+		if request.method=='POST':
+			username = request.POST['username']
+			password = request.POST['password']
+			print password
+			user = User.objects.create_user(username = username, password = password)
+			user.save()
+			return HttpResponseRedirect('/teacher/addStaff/')
+		else:
+			users = User.objects.all()
+		return render(request, 'teacher/addStaff.html',{'users': users})
 	else:
-		users = User.objects.all()
-	return render(request, 'teacher/addStaff.html',{'users': users})
+		return HttpResponseRedirect('/')
 
 
 
