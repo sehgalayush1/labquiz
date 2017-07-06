6f0a2b0ba244f97cc6b1c4db11a701bb7f497c94
saurabh ranjan <saurabhranjan1806@gmail.com>
2017-07-07 01:47:33 +0530

    login student

--- labquiz/student/urls.py
+++ labquiz/student/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name = 'login/home.html')),
+    url(r'^$', TemplateView.as_view(template_name = 'student/home.html')),
 
 ]
